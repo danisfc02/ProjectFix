0b5c528542fb463c72e3d569660c5cac415adfaf
danisfc02 <114477437+danisfc02@users.noreply.github.com>
2023-04-09 15:55:29 +0200

    Add files via upload

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -21,39 +21,41 @@ namespace Factorial
             {
                 Console.WriteLine(numbers[i]);
             }
-            Console.WriteLine("Average: " + average(numbers));
+            Console.WriteLine("Average: " + Average(numbers));
 
             Console.WriteLine("Factorial for: ");
             for (int i = 0; i < numbers.Count; i++)
             {
-                Console.WriteLine(factorial_for((ulong)numbers[i]));
+                Console.WriteLine(Factorial_for((ulong)numbers[i]));
             }
 
             Console.WriteLine("Factorial recursive: ");
             for (int i = 0; i < numbers.Count; i++)
             {
-                Console.WriteLine(factorial_recursive((ulong)numbers[i]));
+                Console.WriteLine(Factorial_recursive((ulong)numbers[i]));
             }
         }
 
-        public static float average(List<float> numbers)
+        public static float Average(List<float> numbers)
         {
             float sum = 0;
+            float average;
+
             for (int i = 0; i < numbers.Count; i++)
             {
                 sum += numbers[i];
             }
-            return sum;
+            return average= sum / numbers.Count;
         }
-        public static ulong factorial_recursive(ulong n)
+        public static ulong Factorial_recursive(ulong n)
         {
             if (n < 1)
             {
-                return factorial_recursive(n - 1);
+                return 1;
             }
-            return n * factorial_recursive(n - 1);
+            return n * Factorial_recursive(n - 1);
         }
-        public static ulong factorial_for(ulong n)
+        public static ulong Factorial_for(ulong n)
         {
             ulong res = 1;
             for (ulong i = 2; i <= n; i++)
